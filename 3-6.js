fd1ea51b2d4291e53fb1a26d8b23949684002511
Fahim-Prodhan <prodhan15-4809@diu.edu.bd>
2026-01-12 23:49:51 +0600

    module 3 video 6 work

---
+++ 3-6.js
@@ -0,0 +1,16 @@
+class Node {
+    constructor(value){
+        this.value = value;
+        this.next = null;
+    }
+}
+
+    const head = new Node(10);
+    head.next = new Node(20);
+    head.next.next = new Node(20);
+    let temp = head;
+
+while(temp !=null){
+    console.log(temp.value, '\n');
+    temp = temp.next;    
+}
